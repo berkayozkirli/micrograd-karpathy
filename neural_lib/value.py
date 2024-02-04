c5bcae7f4bcefccf2755fe7037467445eb5b955c
berkayozkirli <berkay.ozkirli@metu.edu.tr>
2024-02-04 22:47:31 +0300

    Basics of micrograd implemented again, basically just like in the original repository.

---
+++ neural_lib/value.py
@@ -0,0 +1,89 @@
+class Value:
+    # Sinle value
+    def __init__(self, data, _children=(), _op=''):
+        self.data = data
+        self.grad = 0.0
+        # The function that will take us backwards, will be initiliaized in an op function 
+        self._backward = lambda: None
+        # The variable that will be used to build the tree, will be initiliaized in an op function 
+        self._children = _children
+        # The operation that produced this value, will be initiliaized in an op function 
+        self._op = _op
+
+    def __add__(self, other):
+        other = other if isinstance(other, Value) else Value(other)
+        out = Value(self.data + other.data, (self, other), "+")
+        def _backward():
+            self.grad += out.grad
+            other.grad += out.grad
+        out._backward = _backward
+        return out
+
+    def __mul__(self, other):
+        other = other if isinstance(other, Value) else Value(other)
+        out = Value(self.data * other.data, (self, other), "x")
+        def _backward():
+            self.grad += other.data * out.grad
+            other.grad += self.data * out.grad
+        out._backward = _backward
+
+        return out
+
+    def __pow__(self, other):
+        assert isinstance(other, (int, float)), "only supporting int/float powers for now"
+        out = Value(self.data**other, (self,), f'**{other}')
+        def _backward():
+            self.grad += other * (self.data ** (other-1)) * out.grad
+        out._backward = _backward
+
+        return out
+
+    def relu(self):
+        out = Value(0 if self.data < 0 else self.data, (self,), 'ReLU')
+        def _backward():
+            self.grad += 0 if self.data < 0 else 1 * out.grad
+        out._backward = _backward
+
+        return out
+
+    # This function topologically sort the tree of operations to find gradients by going backwards
+    def backward(self):
+        topologically_sorted = []
+        visited = set()
+        def topologically_sort(node):
+            if node not in visited:
+                visited.add(node)
+                for child in node._children:
+                    topologically_sort(child)
+                topologically_sorted.append(node)   
+        topologically_sort(self)
+        # Order in which the backpropagation takes place
+        backwards_order = topologically_sorted.reverse()
+        # Initialize to one, else all will be zero
+        self.grad = 1.0
+        for node in backwards_order:
+            node._backward 
+
+    def __neg__(self): # -self
+        return self * -1
+
+    def __radd__(self, other): # other + self
+        return self + other
+
+    def __sub__(self, other): # self - other
+        return self + (-other)
+
+    def __rsub__(self, other): # other - self
+        return other + (-self)
+
+    def __rmul__(self, other): # other * self
+        return self * other
+
+    def __truediv__(self, other): # self / other
+        return self * other**-1
+
+    def __rtruediv__(self, other): # other / self
+        return other * self**-1
+
+    def __repr__(self):
+        return f"Value(data={self.data}, grad={self.grad})"
